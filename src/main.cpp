6dc901b8f8574fa846fc13bb937f1c5e18fbc011
Arsalan Kazmi <sonicspeed848@gmail.com>
2024-04-14 21:43:56 +0100

    Whatever lmao

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 
 #include <iostream>
 #include <string>
+#include <iomanip>
 
 #include "classes.h"
 #include "interactive.h"
@@ -27,8 +28,58 @@ int main()
 
     std::cout << Strings::horizontalRule(30) << std::endl;
 
-    std::cout << "Name: " << userName << std::endl;
-    std::cout << "Badge Number: " << badgeNumber << std::endl;
+    std::cout << Strings::horizontalRule(30) << std::endl;
+    std::cout << "Welcome officer " << userName << std::endl;
+    std::cout << "The system is ready to use." << std::endl;
+
+    std::cout << Strings::horizontalRule(30) << std::endl;
+
+    std::cout << "Please enter car information, separated by spaces." << std::endl;
+    std::cout << "[Make] [Model] [Colour] [Licence Number]" << std::endl;
+
+    std::string carMake;
+    std::string carModel;
+    std::string carColour;
+    std::string licenceNumber;
+
+    std::cin >> carMake >> carModel >> carColour >> licenceNumber;
+
+    std::cout << "How many minutes of parking were purchased?" << std::endl;
+
+    int purchasedDurationMins;
+
+    std::cin >> purchasedDurationMins;
+
+    std::cout << "How many minutes has the vehicle been parked?" << std::endl;
+
+    int elapsedDurationMins;
+
+    std::cin >> elapsedDurationMins;
+
+    std::cout << Strings::horizontalRule(30) << std::endl;
+
+    std::cout << "Police Officer Information:" << std::endl;
+    std::cout << "  Name: " << userName << std::endl;
+    std::cout << "  Badge Number: " << badgeNumber << std::endl;
+
+    std::cout << "Car Information:" << std::endl;
+    std::cout << "  Make: " << carMake << std::endl;
+    std::cout << "  Model: " << carModel << std::endl;
+    std::cout << "  Colour: " << carColour << std::endl;
+    std::cout << "  Licence Number: " << licenceNumber << std::endl;
+
+    std::cout << "Meter Information:" << std::endl;
+    std::cout << "  Purchased Duration: " << purchasedDurationMins << " minutes" << std::endl;
+    std::cout << "  Elapsed Duration: " << elapsedDurationMins << " minutes" << std::endl;
+
+    double baseFine = 25.0;
+    double additionalFine = 10.0 * (elapsedDurationMins - purchasedDurationMins);
+
+    std::cout << "Ticket Information:" << std::endl;
+    std::cout << "  Minutes In Violation: " << elapsedDurationMins - purchasedDurationMins << " minutes" << std::endl;
+    std::cout << std::fixed << std::setprecision(2) << "  Fine: £" << baseFine + additionalFine << std::endl;
+
+    std::cout << Strings::horizontalRule(30) << std::endl;
 
     return EXIT_SUCCESS;
 
@@ -37,11 +88,11 @@ int main()
 
     ParkedCar myCar;
 
-    std::string carMake = "Toyota";
-    std::string carModel = "Camry";
-    std::string carColour = "Blue";
-    std::string licenceNumber = "ABC123";
-    myCar.setCarInfo(carMake, carModel, carColour, licenceNumber);
+    std::string example_carMake = "Toyota";
+    std::string example_carModel = "Camry";
+    std::string example_carColour = "Blue";
+    std::string example_licenceNumber = "ABC123";
+    myCar.setCarInfo(example_carMake, example_carModel, example_carColour, example_licenceNumber);
 
     myCar.printRelevantInfo();
 
